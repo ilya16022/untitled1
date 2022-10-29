a2ddda92c6018086ae8dd9cc16be3990e014a174
Студент <student@ITStep.local>
2022-10-29 13:21:12 +0300

    Initial commit

---
+++ GDG.py
@@ -0,0 +1,10 @@
+import rondom
+class Human
+    def __init__(self,name = "Sim", job = None, home = None):
+        self.name = name
+        self.job = job
+        self.home = home
+        self. car = car
+        self.gladness = 50
+        self.stiety = 50
+        
